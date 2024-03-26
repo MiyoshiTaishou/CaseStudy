bfefbf0e9b8cd666e06cd34c3f03a2529519240d
リニューアルマン <pokomal2003@gmail.com>
2024-03-26 16:36:54 +0900

    20240326_Sawano_Ver0.1
    S_PlayerChangeに以下の変更を加えました。
    ・変身前、変身後のスプライトを指定しなくても自動的にプレイヤーと敵のスプライトを入れ替えるようにしました。
    ・変身時間の設定ができるようになりました。デフォルトでは10秒です。
    
    また、変身をするための攻撃判定のようなものを設定しました。
    その際、プレイヤー自体にこの当たり判定をつけると、敵のRayが誤認してしまうため、当たり判定としての子オブジェクト「PlayerEyeSight」を追加したプレハブをAsset>Object>Playerに「SawanoPlayer」として追加しました。

--- work/CaseStudy/Assets/Script/Player/S_PlayerChange.cs
+++ work/CaseStudy/Assets/Script/Player/S_PlayerChange.cs
@@ -4,48 +4,94 @@ using UnityEngine;
 
 public class S_PlayerChange : MonoBehaviour
 {
-    /// <summary>
     ///Bボタンが押されたかどうか
-    /// </summary>
-    private
-    bool IsBDown = false;
+    private bool isBDown = false;
 
-    [Header("変身前の見た目"),SerializeField]
-    Sprite defaultsprite;
+    //敵が視界内にいるかどうか
+    private bool isSerch = false;
 
-    [Header("変身後の見た目"),SerializeField]
-    Sprite changedsprite;
+    [Header("変身時間"), SerializeField]
+    float fWaitTime = 10.0f;
 
-    private
-    SpriteRenderer spriteRenderer = null;
+    //変身中かどうか
+    private bool isChanging = false;
+    bool GetisChanging() { return isChanging; }
+
+    //変身前の見た目(Player)
+    private Sprite PlayerSprite;
+
+    //変身後の見た目(Enemy)
+    private Sprite EnemySprite;
+
+    private SpriteRenderer srEnemy = null;
+
+    private SpriteRenderer srPlayer = null;
+
+    private Collider2D colEnemy;
     // Start is called before the first frame update
     void Start()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        if(!spriteRenderer)
+        srPlayer = transform.root.GetComponent<SpriteRenderer>();
+        if(!srPlayer)
         {
             Debug.LogError("SpriteRendererがありません");
         }
+       
     }
 
     // Update is called once per frame
     void Update()
     {
         //変身ボタンが押されたかどうか判定
-        IsBDown = Input.GetKeyDown(KeyCode.C);//ここでキー指定してね
-        if (IsBDown) 
+        isBDown = Input.GetKeyDown(KeyCode.C);//ここでキー指定してね
+        Debug.Log("ボタン押下状況=" + isBDown);
+        if (isBDown&&isSerch) 
+        {
+            srEnemy = colEnemy.GetComponent<SpriteRenderer>();
+            EnemySprite = srEnemy.sprite;
+            StartCoroutine(Change());
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D _collision)
+    {
+        //敵に接触していればその敵の盲目状態と見た目の情報を取得
+        if(_collision.tag=="Enemy")
+        {
+            M_BlindingMove blindingMove =_collision.GetComponent<M_BlindingMove>();
+            isSerch=blindingMove.GetIsBlinding();
+            colEnemy = _collision;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D _collision)
+    {
+        isSerch = false;
+    }
+
+    //コルーチン。
+    IEnumerator Change()
+    {
+        //終わるまで待ってほしい処理を書く
+        //変身中でなければプレイヤーと敵の見た目を入れ替える、変身中であれば変身時間をリセットするだけ
+        if (!isChanging)
         {
             //現在の状況に応じてタグと見た目を変更
-            if (this.tag == "Player")
-            {
-                this.tag = "Enemy";
-                spriteRenderer.sprite = changedsprite;
-            }
-            else if(this.tag=="Enemy")
-            {
-                this.tag = "Player";
-                spriteRenderer.sprite = defaultsprite;
-            }
+            transform.root.tag = "Enemy";
+            PlayerSprite = srPlayer.sprite;
+            //見た目の変更
+            srPlayer.sprite = EnemySprite;
+            srEnemy.sprite = PlayerSprite;
+            isChanging= true;
         }
+        //指定の秒数待つ
+        yield return new WaitForSeconds(fWaitTime);
+        //再開してから実行したい処理を書く
+
+        //見た目を元に戻す
+        srPlayer.sprite = PlayerSprite;
+        srEnemy.sprite = EnemySprite;
+        Debug.Log("変身解除");
+        transform.root.tag = "Player";
+        isChanging= false;
     }
 }
