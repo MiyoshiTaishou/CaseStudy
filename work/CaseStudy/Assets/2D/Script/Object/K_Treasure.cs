48cc9fa9051de11d605c1447aaf5097d6f78048d
mk317 <harukimura0317@gmail.com>
2024-05-06 08:10:09 +0900

    Add::クリア演出(仮)+日報
    宝改修時に画面上部にCLEARって表示されます。半分くらい見切れてます。

---
+++ work/CaseStudy/Assets/2D/Script/Object/K_Treasure.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class K_Treasure : MonoBehaviour
+{
+    [Header("ステージクリア(仮)"), SerializeField]
+    private GameObject StageClearPrefab;
+
+    private GameObject StageClearObj;
+
+    void Start()
+    {
+        StageClearObj = Instantiate(StageClearPrefab, transform.position, Quaternion.identity);
+        StageClearObj.SetActive(false);
+    }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // プレイヤーがぶつかってきたら
+        if (collision.collider.tag == "Player")
+        {
+            Destroy(gameObject);
+
+            StageClearObj.SetActive(true);
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        // プレイヤーがぶつかってきたら
+        if (collision.collider.tag == "Player")
+        {
+            Destroy(gameObject);
+
+            StageClearObj.SetActive(true);
+        }
+    }
+}
