5ff7311f7763026737106199fcf3492d7b867411
リニューアルマン <pokomal2003@gmail.com>
2024-05-10 23:51:08 +0900

    敵玉アニメカット

--- work/CaseStudy/Assets/2D/Script/Enemy/S_EnemyBallAnimation.cs
+++ work/CaseStudy/Assets/2D/Script/Enemy/S_EnemyBallAnimation.cs
@@ -13,25 +13,25 @@ public class S_EnemyBallAnimation : MonoBehaviour
         animator = GetComponent<Animator>();
 
         // アニメーターのパラメーターを設定し、アニメーションを再生する
-        animator.Play("enemy_roll_start");
-        //animator.Play("enemy_roll_loop");
+        //animator.Play("enemy_roll_start");
         animator.SetBool("roll", true);
+        animator.Play("enemy_roll_loop");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!animator.GetCurrentAnimatorStateInfo(0).IsName("enemy_roll_start") &&
-            animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
-        {
-            // アニメーションが終了したら何かをする
-            //Destroy(transform.Find("w").gameObject);
-            transform.Find("w").gameObject.SetActive(false);
-            Vector3 scale= transform.localScale;
-            scale.x = 0.5f;
-            scale.y = 0.5f;
-            transform.localScale = scale;
-            animator.SetTrigger("enemy_roll_loop");
-        }
+        //if (!animator.GetCurrentAnimatorStateInfo(0).IsName("enemy_roll_start") &&
+        //    animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
+        //{
+        //    // アニメーションが終了したら何かをする
+        //    //Destroy(transform.Find("w").gameObject);
+        //    transform.Find("w").gameObject.SetActive(false);
+        //    Vector3 scale= transform.localScale;
+        //    scale.x = 0.5f;
+        //    scale.y = 0.5f;
+        //    transform.localScale = scale;
+        //    animator.SetTrigger("enemy_roll_loop");
+        //}
     }
 }
