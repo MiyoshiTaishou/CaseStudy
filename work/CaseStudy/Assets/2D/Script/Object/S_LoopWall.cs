4c5904353c9066d19fdcbad25f49d0079907ebde
リニューアルマン <pokomal2003@gmail.com>
2024-06-07 20:31:41 +0900

    osidasi

--- work/CaseStudy/Assets/2D/Script/Object/S_LoopWall.cs
+++ work/CaseStudy/Assets/2D/Script/Object/S_LoopWall.cs
@@ -19,6 +19,14 @@ public class S_LoopWall : MonoBehaviour
     private AudioSource audioSource= null;
     public bool GetiswarpRight() { return iswarpRight; }
 
+    GameObject ColObject = null;
+    float speedx = 0;
+
+    /// <summary>
+    /// 速度を保存するための変数
+    /// </summary>
+    Vector3 vel;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -33,6 +41,14 @@ public class S_LoopWall : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Vector2 vel2 = collision.GetComponent<Rigidbody2D>().velocity;
+        vel = vel2;
+        speedx= vel2.x;
+        Debug.Log("ｓぴーーーーーーーーーど" + speedx);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -52,20 +68,55 @@ public class S_LoopWall : MonoBehaviour
                 newpos.x -= 2.0f;
             }
             audioSource.PlayOneShot(audioclip);
-            collision.gameObject.transform.position=newpos;
+            collision.gameObject.transform.position=newpos;            
+        }
+        else if (isWarped == false && OK == false && 
+            collision.collider.CompareTag("EnemyBall"))
+        {
+            Rigidbody2D rb = collision.collider.GetComponent<Rigidbody2D>();
+            speedx=rb.velocity.x;
+            StartCoroutine(HitStop());
+            Vector3 newpos = warpObj.transform.position;
+            if (warpObj.GetComponent<S_LoopWall>().GetiswarpRight() == true)
+            {
+                newpos.x += 2.0f;
+            }
+            else if (warpObj.GetComponent<S_LoopWall>().GetiswarpRight() == false)
+            {
+                newpos.x -= 2.0f;
+            }
+            audioSource.PlayOneShot(audioclip);
+            collision.gameObject.transform.position = newpos;
+
+            //vel = collision.gameObject.GetComponent<Rigidbody2D>().velocity;
         }
     }
 
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if(collision.collider.CompareTag("EnemyBall"))
+        {
+            Debug.Log("すすめ");
+            Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();              
+            rb.velocity = vel;
+        }
+    }
 
     IEnumerator HitStop()
     {   
         isWarped = true;
         warpObj.GetComponent<S_LoopWall>().isWarped= true;
         //指定のフレーム待つ
-        yield return new WaitForSecondsRealtime(1);
+        yield return new WaitForSecondsRealtime(0.2f);
         Debug.Log("ああああ");
         isWarped = false;
         warpObj.GetComponent<S_LoopWall>().isWarped = false;
-
+        //if(ColObject.CompareTag("EnemyBall"))
+        //{
+        //    Rigidbody2D rb = ColObject.GetComponent<Rigidbody2D>();
+        //    Vector2 vel = rb.velocity;
+        //    vel.x = speedx;
+        //    rb.velocity = vel;
+        //}
     }
 }
