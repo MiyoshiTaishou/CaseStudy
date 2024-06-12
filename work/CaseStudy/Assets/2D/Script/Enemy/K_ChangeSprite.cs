0da6d15d2625b1cf7c15d384866f0fc51e0b53d5
ninomiyaren <ninomiyaren1013@gmail.com>
2024-06-12 14:05:39 +0900

    敵のぴよぴよ
    転がされた敵のvelocityが1以下になったらピヨピヨするようにした

--- work/CaseStudy/Assets/2D/Script/Enemy/K_ChangeSprite.cs
+++ work/CaseStudy/Assets/2D/Script/Enemy/K_ChangeSprite.cs
@@ -18,6 +18,8 @@ public class K_ChangeSprite : MonoBehaviour
 
     private S_EnemyBall EnemyBall;
 
+    private Animator animator;
+
     private void Start()
     {
         string strEnemySprites = EnemyObjNormal.name;
@@ -29,18 +31,42 @@ public class K_ChangeSprite : MonoBehaviour
             EnemySpriteRolling.transform.parent = this.transform;
         }
         EnemyBall = GetComponent<S_EnemyBall>();
+
+        animator = transform.GetChild(2).GetComponent<Animator>();
     }
 
     private void Update()
     {
         bool IsRolling = EnemyBall.GetisBall();
+        bool isHitStop = EnemyBall.GetisHitStop();
+        bool isMoving = gameObject.GetComponent<Rigidbody2D>().velocity.x >= 1.0f;
+
+        // ヒットストップ中はvelocityが0になるので強制的に
+        if (isHitStop)
+        {
+            isMoving = true;
+        }
         if (IsRolling)
         {
-            EnemySpriteNormal.SetActive(false);
-            EnemySpriteRolling.SetActive(true);
-            Vector3 pos=this.transform.position;
-            pos.y += 0.3f;
-            EnemySpriteRolling.transform.position = pos;
+            // 動いているなら
+            if (isMoving)
+            {
+                EnemySpriteNormal.SetActive(false);
+                EnemySpriteRolling.SetActive(true);
+                Vector3 pos = this.transform.position;
+                pos.y += 0.3f;
+                EnemySpriteRolling.transform.position = pos;
+                animator.SetBool("rollover", false);
+
+            }
+            // 転がっていない
+            else
+            {
+                EnemySpriteNormal.SetActive(true);
+                EnemySpriteNormal.transform.position = new Vector3(transform.position.x, EnemySpriteNormal.transform.position.y, transform.position.z);
+                EnemySpriteRolling.SetActive(false);
+                animator.SetBool("rollover", true);
+            }
         }
         else
         {
