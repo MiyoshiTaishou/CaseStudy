bd9906ac98d4b5ae64504b8657f79340c1af2fc0
mk317 <harukimura0317@gmail.com>
2024-06-30 04:38:00 +0900

    タイトルシーン修正
    UI位置と点滅表示

--- work/CaseStudy/Assets/2D/Script/UI/M_TitleButtonPopUp.cs
+++ work/CaseStudy/Assets/2D/Script/UI/M_TitleButtonPopUp.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class M_TitleButtonPopUp : MonoBehaviour
 {
@@ -10,16 +11,26 @@ public class M_TitleButtonPopUp : MonoBehaviour
     [Header("表示するオブジェクト"), SerializeField]
     private GameObject[] buttons;
 
+    private Color col;
+    private float time;
+    private float fAlpha;
+
     // Start is called before the first frame update
     void Start()
     {
         m_Title.GetComponent<M_TitleSelect>().enabled = false;
+        col = GetComponent<Image>().color;
+        fAlpha = 0.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("SympathyButton"))
+        time += Time.deltaTime;
+        fAlpha =Mathf.PingPong(time, 1.0f);
+        col.a = fAlpha;
+        GetComponent<Image>().color = col;
+        if (Input.GetButtonDown("SympathyButton"))
         {
             foreach(var button in buttons)
             {
